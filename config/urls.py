9a9c2cfdc439d9bb8169e595d83e3742fd152e33
vladmaster3455 <sergesenghor@esp.sn>
2026-05-11 11:20:00 +0200

    set up django project

---
+++ config/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/accounts/", include("apps.accounts.urls")),
+    path("api/resumes/", include("apps.resumes.urls")),
+    path("api/analytics/", include("apps.analytics.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
